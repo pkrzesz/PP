4275950ca1681ff86a5d5269b6bca0585bddca6c
Piotr <piotr.a.krzesz@gmail.com>
2023-10-22 16:22:36 +0200

    version1.1 python

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import random
 random.seed()
 
+def odpowiedz(lista):
+    answer = input("What is your answer?\n")
+    if answer.lower() == lista.correct:
+        return "CORRECT"
+    elif answer.lower() in ["a", "b", "c", "d"]:
+        return "INCORRECT"
+    else:
+        print("Answer outside of range, try again")
+        return odpowiedz(lista)
+
 class pytanie:
     def __init__(self, question, a, b, c, d, correct):
         self.question = question
@@ -15,19 +25,17 @@ class pytanie:
         print(self.a, self.b)
         print(self.c, self.d)
 
-list = [pytanie("test?", "A", "b", "c", "d", "a"), pytanie("test?", "a", "B", "c", "d", "b"), pytanie("test?", "a", "b", "C", "d", "c"), pytanie("test?", "a", "b", "c", "D", "d")]
+listoflists = [[pytanie("test?", "A", "b", "c", "d", "a"),
+                pytanie("test?", "a", "B", "c", "d", "b"),
+                pytanie("test?", "a", "b", "C", "d", "c"),
+                pytanie("test?", "a", "b", "c", "D", "d")],
+               [pytanie("test2a?", "2A", "b", "c", "d", "a"),
+                pytanie("test2b?", "a", "2B", "c", "d", "b")]]
 
-x = random.randint(0, len(list)-1)
-
-list[x].print()
-def odpowiedz(lista, x):
-    answer = input("What is your answer?\n")
-    if answer.lower() == lista[x].correct:
-        print("CORRECT")
-    elif answer.lower() in ["a", "b", "c", "d"]:
-        print("INCORRECT")
-    else:
-        print("Answer outside of range, try again")
-        odpowiedz(lista, x)
-
-odpowiedz(list, x)
+for list in listoflists:
+    x = random.randint(0, len(list)-1)
+    list[x].print()
+    y = odpowiedz(list[x])
+    print(y)
+    if y == "INCORRECT":
+        break
