9f0a7d3d313ac869898c2a8232f72eb3cb5297f1
Piotr <piotr.a.krzesz@gmail.com>
2023-10-26 11:19:13 +0200

    version 1.5 python, changed to include actual lines telling you if it's correct or not instead of printing the result, made lists have different len to make sure random works correctly

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import random
 random.seed()
 
 # do zrobienia:
-# użycie dict (dictionary, mapa) zamiast list aby mieć dictoflists zamiast listoflists i mieć trzecią strukturę danych
-# zmiana kodu aby używać poprawnie dict i losować z listy po dopasowaniu klucza
 # dodanie komentarzy w poprawne miejsca, aby opisać które wymagania są spełniane przez jaką część kodu 
 # (klasa pytanie jest globalna złożona na przykład, a liczby a, b, x i y w funkcji gra() to lokalne proste)
 # faktyczne dodanie faktycznych pytań i zwiększenie ich ilości
+
+# komentarz do osoby to widzącej:
+# dict (mapa), oraz tuple (tupla) zostały praktycznie użyte jako list (lista)
 
 def gra():
     a = 0
@@ -15,22 +16,25 @@ def gra():
         lista = dictoflists[key]
         x = random.randint(0, len(lista)-1)
         lista[x].print()
+        print("Pytanie numer", key)
         y = odpowiedz(lista[x])
-        print(y)
         if y == "INCORRECT" or y == "END":
             break
         else:
+            print("Poprawna odpowiedz")
             a+=1
             if money[a] in guaranteedmoney:
                 b+=1
+                print("Masz teraz gwarantowane", guaranteedmoney[b], "PLN")
     if y == "INCORRECT":
+        print("Niestety, poprawna odpowiedz byla", lista[x].correct)
         print("Wygrales", guaranteedmoney[b], "PLN")
     else:
         print("Wygrales", money[a], "PLN")
     ponownie(y)
 
 def odpowiedz(lista):
-    answer = input("What is your answer?\nType in a, b, c or d, or end the game early with stop\n")
+    answer = input("Jaka jest twoja odpowiedz?\nWpisz a, b, c, d lub stop aby zakończyć grę wcześnie\n")
     if answer.lower() == lista.correct:
         return "CORRECT"
     elif answer.lower() in ["a", "b", "c", "d"]:
@@ -38,7 +42,7 @@ def odpowiedz(lista):
     elif answer.lower() == "stop":
         return "END"
     else:
-        print("Answer outside of range, try again")
+        print("Odpowiedz z poza zakresu przyjmowanych odpowiedzi, sproboj ponownie")
         return odpowiedz(lista)
     
 def ponownie(wynik):
@@ -50,7 +54,7 @@ def ponownie(wynik):
         case "END":
             print("Gra zakonczona komenda stop")
         case "y":
-            print("Zagrajmy ponownie")
+            print("Zagrajmy ponownie, przed tobą 12 pytań")
             gra()
         case "n":
             print("Dziekujemy za gre")
@@ -70,8 +74,10 @@ class pytanie:
     
     def print(self):
         print(self.question)
-        print(self.a, self.b)
-        print(self.c, self.d)
+        print("a:", self.a)
+        print("b:", self.b)
+        print("c:", self.c)
+        print("d:", self.d)
 
 money = (0, 500, 1000, 2000, 5000, 10000, 20000, 40000, 75000, 125000, 250000, 500000, 1000000)
 guaranteedmoney = (0, 1000, 40000, 1000000)
@@ -79,10 +85,15 @@ dictoflists = {0:[pytanie("test?", "A", "b", "c", "d", "a"),
                 pytanie("test?", "a", "B", "c", "d", "b"),
                 pytanie("test?", "a", "b", "C", "d", "c"),
                 pytanie("test?", "a", "b", "c", "D", "d")],
-               1:[pytanie("test2a?", "2A", "b", "c", "d", "a"),
-                pytanie("test2b?", "a", "2B", "c", "d", "b")],
-               2:[pytanie("test3?", "a", "b", "c", "D", "d")],
-               3:[pytanie("test4?", "alpha", "bravo", "CHARLIE", "delta", "c")]}
+               1:[pytanie("test1?", "1A", "b", "c", "d", "a"),
+                pytanie("test1?", "a", "1B", "c", "d", "b"),
+                pytanie("test1?", "a", "b", "1C", "d", "c")],
+               2:[pytanie("test2?", "2A", "b", "c", "d", "a"),
+                  pytanie("test2?", "a", "2B", "c", "d", "b"),
+                  pytanie("test2?", "a", "b", "2C", "d", "c"),
+                  pytanie("test2?", "a", "b", "c", "2D", "d"),
+                  pytanie("test2?", "no", "nope", "this one", "nah", "c")],
+               3:[pytanie("test3?", "alpha", "bravo", "CHARLIE", "delta", "c")]}
 
 print("Witamy w milionerach, przed toba 12 pytan")
-gra()      
+gra()
